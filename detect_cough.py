9e2b7e67eea1ffc734a55c613e713cffe72bcfc8
marmoh2002 <mhabiba2002@gmail.com>
2025-07-30 05:46:43 +0300

    basic changes  xgb

--- detect_cough.py
+++ detect_cough.py
@@ -17,12 +17,12 @@ from src.DSP import classify_cough
 from scipy.io import wavfile
 import pickle
 import argparse
-
+import xgboost as xgb
 
 def main(input_file):
-    print("Current working directory:", os.getcwd())
-    print("Script location:", os.path.abspath(__file__))
-    print("Script directory:", os.path.dirname(os.path.abspath(__file__)))
+    # print("Current working directory:", os.getcwd())
+    # print("Script location:", os.path.abspath(__file__))
+    # print("Script directory:", os.path.dirname(os.path.abspath(__file__)))
     """
     Detect cough in a given audio file
     Inputs:
@@ -36,7 +36,14 @@ def main(input_file):
         'rb'))
     scaler = pickle.load(open(os.path.join(script_dir, '/models',
         'cough_classification_scaler'), 'rb'))
-
+    model_path = os.path.join(script_dir, 'models', 'cough_classifier.json')  # or .ubj or .bin
+    model = xgb.Booster()
+    model.load_model(model_path)
+    # Load the scaler
+    scaler_path = os.path.join(script_dir, 'models', 'cough_classification_scaler.pkl')
+    with open(scaler_path, 'rb') as f:
+        scaler = pickle.load(f)
+        
     fs, x = wavfile.read(input_file)
     prob = classify_cough(x, fs, model, scaler)
     print(f"{input_file} has probability of cough: {prob}")
