34fc4b3ebf013f10af708cbb85113d851b006134
marmoh2002 <mhabiba2002@gmail.com>
2025-07-30 05:06:08 +0300

    basic changes

--- segment_cough.py
+++ segment_cough.py
@@ -5,7 +5,7 @@
 import librosa
 import os
 import sys
-sys.path.append('.../src/')
+sys.path.append('/src')
 sys.path.append(os.path.abspath('.../src/cough_detection'))
 from segmentation import segment_cough
 import soundfile as sf
