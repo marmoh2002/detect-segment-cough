56591d599db7695acdc7321a932ede182825596a
marmoh2002 <mhabiba2002@gmail.com>
2025-07-30 05:38:59 +0300

    basic changes  debug

--- detect_cough.py
+++ detect_cough.py
@@ -32,7 +32,7 @@ def main(input_file):
     """
     # data_folder = './sample_recordings'
     script_dir = os.path.dirname(os.path.abspath(__file__))
-    model = pickle.load(open(os.path.join(script_dir, '/models', 'cough_classifier'),
+    model = pickle.load(open(os.path.join(script_dir, 'models', 'cough_classifier'),
         'rb'))
     scaler = pickle.load(open(os.path.join(script_dir, '/models',
         'cough_classification_scaler'), 'rb'))
