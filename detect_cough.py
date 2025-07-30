a93586e58cd237d99eb296f504132d339c8329b4
marmoh2002 <mhabiba2002@gmail.com>
2025-07-30 05:23:00 +0300

    basic changes

--- detect_cough.py
+++ detect_cough.py
@@ -6,6 +6,8 @@ import os
 import sys
 sys.path.append(os.path.abspath('.../src'))
 sys.path.append(os.path.abspath('.../src/cough_detection'))
+sys.path.append(os.path.abspath('./src/'))
+sys.path.append(os.path.abspath('./models/'))
 from src.feature_class import features
 from src.DSP import classify_cough
 from scipy.io import wavfile
